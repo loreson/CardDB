2e8a98f499072960061749a8b5e32d7b7b12d298
Christian Vögl <christian.voegl@fau.de>
2017-12-18 20:57:08 +0100

    ScannedCard.java

---
+++ src/cardDB/ScannedCard.java
@@ -0,0 +1,13 @@
+package cardDB;
+
+public class ScannedCard{
+	private String name;
+	private String edition;
+	private Condition condition;
+	private Language language
+	private boolean foil;
+	public ScannedCard(String name, String editon, Condition condition, Language language, boolean foil)
+	{
+		this.name=name; this.edition=edition; this.condition=condition; this.language=language;this.foil=foil;
+	}
+}

40f4e7f6030a67f02e272da8935ab1277c6d992d
Christian Vögl <christian.voegl@fau.de>
2017-12-19 07:42:45 +0100

    adde prodNr, TODO

--- src/cardDB/ScannedCard.java
+++ src/cardDB/ScannedCard.java
@@ -6,8 +6,10 @@ public class ScannedCard{
 	private Condition condition;
 	private Language language
 	private boolean foil;
+	private int prodNr;
 	public ScannedCard(String name, String editon, Condition condition, Language language, boolean foil)
 	{
 		this.name=name; this.edition=edition; this.condition=condition; this.language=language;this.foil=foil;
+		//TODO: get prodNr from mkm
 	}	
 }
